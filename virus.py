a87d0c5e1f907a821b2ddb6f8278e417631979a1
Shan Shen <shanshen@macbook-air.ad.bcm.edu>
2018-08-14 16:56:15 -0500

    Modifications based on Jake's comments.

--- virus.py
+++ virus.py
@@ -8,23 +8,23 @@ class ConstructSource(dj.Lookup):
     definition = """
     construct_source : varchar(64) # source of the construct
     """
-    contents = [['Addgene'], ['Constom made']]
+    contents = [['Addgene'], ['Custom']]
 
 @schema
 class Construct(dj.Manual):
-    # information of the construct
+    # a construct containing multiple genetic elements
     definition = """
-    construct_nickname : varchar(255)    # nickname of a construct
+    construct_nickname:     varchar(255)    # nickname of the construct
     ---
-    construct_fullname : varchar(255)   # full anme of a construct 
+    construct_fullname:     varchar(255)    # full name of the construct 
     -> ConstructSource
-    construct_notes = null : varchar(255)   # notes of the construct
+    construct_notes = null: varchar(255)    # notes about the construct
     """
 
 @schema
 class GeneticElementType(dj.Lookup):
     definition = """
-    genetic_element_type : varchar(64)  # type of the gene element 
+    genetic_element_type : varchar(64)  # type of the genetic element 
     """
     contents = [
         ['Promoter'],
@@ -42,12 +42,12 @@ class GeneticElementType(dj.Lookup):
 class GeneticElement(dj.Lookup):
     # list of gene elements
     definition = """
-    genetic_element_nickname : varchar(32)  # nickname of the genetic element, such as GFP 
+    genetic_element_name:           varchar(32)    # name of the genetic element, such as GFP 
     ---
-    genetic_element_fullname : varchar(256) # full name of a genetic element, such as green fluorescent protein
+    genetic_element_fullname:       varchar(256)   # full name of a genetic element, such as green fluorescent protein
     -> GeneticElementType
-    function_description = null : varchar(1024)  # description of the function of the gene
-    genetic_element_source = null  : varchar(256)   # source of the genetic element
+    function_description = null:    varchar(1024)  # description of the function of the gene
+    genetic_element_source = null:  varchar(256)   # source of the genetic element
     """
 
 @schema
@@ -62,11 +62,11 @@ class ConstructGeneticElement(dj.Manual):
 class VirusType(dj.Lookup):
     # type of virus
     definition = """
-    virus_type : varchar(32)  # type of virus
+    virus_type:                     varchar(32)               # type of virus
     ---
-    virus_type_full_name: varchar(64) # full name of a virus type
-    virus_type_description = null : varchar(256)  # description of the virus type
-    virus_bsl: enum('1', '2', '3', '4')  # biosafety level of the virus type
+    virus_type_full_name:           varchar(64)               # full name of a virus type
+    virus_type_description = null:  varchar(256)              # description of the virus type
+    virus_bsl:                      enum('1', '2', '3', '4')  # biosafety level of the virus type
     """
     contents = [
         ['AAV', 'Adeno-associated virus', '', '1'],
@@ -96,8 +96,9 @@ class Serotype(dj.Lookup):
         ['AAV-DJ', 'Hybid capsid from 8 AAV serotypes, high infection rate across a broad range of cells and tissues.'],
         ['AAV-DJ8', 'A mutant of AAV-DJ that exhibits increased uptake in brain tissues in vivo.'],
         ['AAV2-Retro', 'Retrograde tracing AAV'],
-        ['Unpseudotyped Rabies', 'G deleted rabies, unpseudotyped, does not transmit across synapses'],
+        ['Standard Rabies', 'G deleted rabies, unpseudotyped'],
         ['EnvA Rabies', 'G deleted rabies pseudotyped with EnvA'],
+        ['EnvB Rabies', 'G deleted rabies pseudotyped with EnvB'],
         ['HSV-1', ''],
         ['Other', '']
     ]
@@ -114,40 +115,40 @@ class VirusSource(dj.Lookup):
 class Virus(dj.Manual):
     # information of virus
     definition = """
-    virus_id: int  # virus identifier
+    virus_id:               int          # virus identifier
     ---
-    virus_name : varchar(256) # nickname of a virus
+    virus_name:             varchar(256) # nickname of a virus
     -> Construct
     -> VirusType
     -> Serotype
     -> VirusSource
-    virus_catalog = null :  varchar(64)  # catalog number of the virus
-    virus_lot = null    :  varchar(64)  # lot number of the virus
-    virus_volume = null :  smallint     # volume of the virus, in uL
-    virus_titer = null  :  varchar(64)  # titer of the virus
-    virus_date = null   :  date         # date the virus made
-    virus_notes = null  :  varchar(256) # notes
+    virus_catalog = null:   varchar(64)  # catalog number of the virus
+    virus_lot = null:       varchar(64)  # lot number of the virus
+    virus_volume = null:    smallint     # volume of the virus, in uL
+    virus_titer = null:     varchar(64)  # titer of the virus
+    virus_date = null:      date         # date the virus made
+    virus_notes = null:     varchar(256) # notes
     """
 
 @schema
 class Aliquoting(dj.Manual):
-    # information of aliquote
+    # information of aliquot
     definition = """
     -> Virus
-    aliquoting_date : date      # date when the aliquoting happens
+    aliquoting_date:            date            # date when the aliquoting happens
     ---
-    aliquoting_person : varchar(64)  # person that aliquots the virus
-    amount_per_aliquot : smallint  # in uL
-    aliquoting_number : smallint  # number of aliquots
-    aliquoting_notes = null : varchar(256) # notes
+    aliquoting_person:          varchar(64)     # person that aliquots the virus
+    amount_per_aliquot:         smallint        # in uL
+    aliquoting_number:          smallint        # number of aliquots
+    aliquoting_notes = null:    varchar(256)    # notes
     """
 
 @schema
 class Aliquot(dj.Manual):
     definition = """
     -> Aliquoting
-    aliquot_idx : smallint  # idx of this aliquot
+    aliquot_idx:                smallint         # idx of this aliquot
     ---
-    aliquot_thaw_date = null : date # date the aliquot is thawed
-    aliquot_notes = null : varchar(256) # notes
+    aliquot_thaw_date = null:   date             # date the aliquot is thawed
+    aliquot_notes = null:       varchar(256)     # notes
     """ 
